18eb4b2ae56b4d5e2831258c8c7159cdccc0e9b3
kumorning <72860583+kumorning@users.noreply.github.com>
2020-10-14 05:57:29 -0400

    Add files via upload

---
+++ Everday Object2.py
@@ -0,0 +1,14 @@
+  everyday object = "TV"
+  location ="Living room"
+  energy type = "Electricty"
+  charge type = "Code"
+  Size = 60
+  donation = true
+
+print (everyday object)
+print (location)
+print (energy type)
+print (charge type)
+print (size)
+print (used)
+
---
+++ Everday Object4.py
@@ -0,0 +1,11 @@
+  everyday object = "Muji ball pen"
+  color = "#000000"
+  type = "Smooth gel ink"
+  ball point = 0.5
+  place of product = "Japan"
+
+print (everyday object)
+print (color)
+print (type)
+print (ball point)
+print (place of product)
