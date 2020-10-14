ed2eaacf79b5847c6bec6ef5bba5512a3d8a3b2f
kumorning <72860583+kumorning@users.noreply.github.com>
2020-10-14 05:46:24 -0400

    Add files via upload

---
+++ Everday Object.py
@@ -0,0 +1,13 @@
+  everyday object = "Sous vide machine"
+  location ="Kitchen"
+  energy type = "Electricty"
+  charge = "Code"
+  capacity = 12
+  used = true
+
+print (everyday object)
+print (location)
+print (energy type)
+print (charge type)
+print (capacity)
+print (used)
